7e96b950d17ed355444e17bb1e847c13761d58c9
Antonin <antonin63170@gmail.com>
2025-07-02 16:18:45 +0200

    ✅ Refactor header & update homepage hero

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata, Viewport } from "next";
-import Header from "../components/Header";
+import Header from "../components/header/Header";
 import "../styles/globals.css";
 import "../styles/layout.css";
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Studio Podcast",
   description:
-    "This is an example page showcasing placeholder content and demonstrating the app structure.",
+    "Trouvez et louez votre studio d'enregistrement pour podcast, shooting photo ou production vidéo. Découvrez les meilleures adresses à Paris, Lyon, Marseille et plus encore.",
   robots: {
     index: true,
     follow: true,
@@ -15,8 +15,18 @@ export const metadata: Metadata = {
 const HomePage = () => {
   return (
     <main>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is a simple content area for the home page.</p>
+      <section className="hero">
+        <div className="text-container gap-16">
+          <h1 className="font-heading text-5xl leading-none text-center">
+            Votre plateforme de référence pour la location de studios en France
+          </h1>
+          <p className="text-lg leading-normal text-center text-grey-2">
+            Studio Podcast est l’annuaire de référence pour trouver et réserver
+            des studios podcast, photo et vidéo en France. Trouvez le lieu idéal
+            pour vos projets créatifs, partout en France.
+          </p>
+        </div>
+      </section>
     </main>
   );
 };
