cbed7a88915833aec68af23ead68ccffb899476f
Antonin <antonin63170@gmail.com>
2025-07-16 22:42:27 +0200

    Add Location Studio Page & ContentHero Component

---
+++ src/components/contentSections/ContentHero.tsx
@@ -0,0 +1,29 @@
+export default function ContentHero({
+  title = "Titre",
+  description = "Description",
+  coverImg = "/images/og-cover.webp",
+}) {
+  return (
+    <section className="max-w-md justify-center gap-50">
+      <div className="text-container gap-16">
+        <h1 className="font-heading text-5xl leading-none text-center">
+          {title}
+        </h1>
+      </div>
+      <img
+        className="rounded-6"
+        style={{ objectFit: "cover", width: "100%", height: "540px" }}
+        src={coverImg}
+        alt={title}
+      />
+      <div className="text-container gap-16">
+        <p
+          className="text-lg leading-normal text-grey-2"
+          style={{ whiteSpace: "pre-line" }}
+        >
+          {description}
+        </p>
+      </div>
+    </section>
+  );
+}
