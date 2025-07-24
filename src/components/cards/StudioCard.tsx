32e70cd623883262e28156cb9dac91aab7cc40ed
Antonin <antonin63170@gmail.com>
2025-07-24 12:35:08 +0200

    Fix StudioCard image and layout rendering

--- src/components/cards/StudioCard.tsx
+++ src/components/cards/StudioCard.tsx
@@ -17,11 +17,7 @@ interface Studio {
 export default function StudioCard({ studio }: { studio: Studio }) {
   return (
     <a href="#" className={`${styles.card} flex flex-column rounded-6`}>
-      <img
-        src="{studio.image}"
-        alt="{studio.title}"
-        className={styles.cardCover}
-      />
+      <img src={studio.image} alt={studio.title} className={styles.cardCover} />
       <div className="flex flex-column p-20 gap-16">
         <div className="flex gap-16 justify-between">
           <h3 className="text-2xl text-green">{studio.title}</h3>
@@ -32,16 +28,17 @@ export default function StudioCard({ studio }: { studio: Studio }) {
 
         <p className="text-sm text-grey">{studio.description}</p>
 
-        <div className="flex flex-wrap gap-8">
-          <div>
+        <div className="flex items-center flex-wrap gap-8">
+          <div className="flex items-center">
             <div className={styles.iconRating}>
               <IconStar />
             </div>
             <p className="text-sm">{studio.rating}</p>
           </div>
-          <p className="text-sm">
-            | {studio.size} m² | {studio.capacity} pers.
-          </p>
+          <p className="text-sm">|</p>
+          <p className="text-sm">{studio.size} m²</p>
+          <p className="text-sm">|</p>
+          <p className="text-sm">{studio.capacity} pers.</p>
         </div>
 
         <div className="flex flex-wrap gap-16">
