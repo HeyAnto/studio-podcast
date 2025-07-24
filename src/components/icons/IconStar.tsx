98c78bbea9be6d3e91444a8e6a5a7ba3e74b0bf6
Antonin <antonin63170@gmail.com>
2025-07-24 12:15:12 +0200

    Add IconStar SVG component

---
+++ src/components/icons/IconStar.tsx
@@ -0,0 +1,20 @@
+import { SVGProps } from "react";
+
+export function IconStar(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      {/* Icon from Material Symbols Light by Google - https://github.com/google/material-design-icons/blob/master/LICENSE */}
+      <path
+        fill="currentColor"
+        d="m7.325 18.923l1.24-5.313l-4.123-3.572l5.431-.47L12 4.557l2.127 5.01l5.43.47l-4.123 3.572l1.241 5.313L12 16.102z"
+      />
+    </svg>
+  );
+}
+export default IconStar;
